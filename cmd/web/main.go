d01b3c94de58cb0b4efd5b1a8ef1a603a1eadb2e
Владимир Боговик <cashdivan@gmail.com>
2021-12-23 21:55:49 +0700

    Доступ к статическим файлам. Добавлен обработчик доступа к статическим файлам Добавлена обёртка над файловой системой, с реализацией запрета на просмотр содержимого папки, с условием, что внутри нет index.html

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
@@ -14,10 +15,40 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// отдача статических файлов
+	fileServer := http.FileServer(safeFileSystem{fs: http.Dir("./ui/static")})
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Запуск веб-сервера на http://localhost:4000")
 	err := http.ListenAndServe(":4000", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+}
+
+type safeFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs safeFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+			return nil, err
+		}
+	}
+
+	return f, nil
 }
