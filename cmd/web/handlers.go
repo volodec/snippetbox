56bc0370acb69d4a672af6fa7807842cc202849a
Владимир Боговик <cashdivan@gmail.com>
2022-05-17 21:50:49 +0700

    - реализация получения списка записей и приведение их к структуре модели  - добавление создание записи через транзакцию

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"github.com/volodec/snippetbox/pkg/models"
-	"html/template"
 	"net/http"
 	"strconv"
 )
@@ -66,20 +65,28 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	files := []string{
-		"./ui/html/home.page.tmpl",
-		"./ui/html/base.layout.tmpl",
-		"./ui/html/footer.partial.tmpl",
-	}
-
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
-
-	err = ts.Execute(w, nil)
+	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
 	}
+
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%v\n", snippet)
+	}
+
+	//files := []string{
+	//	"./ui/html/home.page.tmpl",
+	//	"./ui/html/base.layout.tmpl",
+	//	"./ui/html/footer.partial.tmpl",
+	//}
+	//
+	//ts, err := template.ParseFiles(files...)
+	//if err != nil {
+	//	app.serverError(w, err)
+	//	return
+	//}
+	//
+	//err = ts.Execute(w, nil)
+	//if err != nil {
+	//	app.serverError(w, err)
 }
--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -13,19 +13,28 @@ type SnippetModel struct {
 
 // Insert - Метод для создания новой заметки в базе дынных.
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
+	// открытие транзакции
+	tx, err := m.DB.Begin()
+	if err != nil {
+		return 0, err
+	}
+
 	query := `INSERT INTO snippets (title, content, created_at, expires_at)
 	VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
 
-	result, resultErr := m.DB.Exec(query, title, content, expires)
+	result, resultErr := tx.Exec(query, title, content, expires)
 	if resultErr != nil {
+		tx.Rollback()
 		return 0, resultErr
 	}
 
 	id, err := result.LastInsertId()
 	if err != nil {
+		tx.Rollback()
 		return 0, err
 	}
 
+	tx.Commit()
 	return int(id), nil
 }
 
@@ -55,5 +64,43 @@ func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
 
 // Latest - Метод возвращает 10 наиболее часто используемые заметки.
 func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
-	return nil, nil
+	query := `SELECT id, title, content, created_at, expires_at FROM snippets
+	WHERE expires_at > UTC_TIMESTAMP() ORDER BY created_at LIMIT 10`
+
+	rows, err := m.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+
+	// Откладываем вызов rows.Close(), чтобы быть уверенным, что набор результатов из sql.Rows
+	// правильно закроется перед вызовом метода Latest(). Этот оператор откладывания
+	// должен выполнится ПОСЛЕ проверки на наличие ошибки в методе Query().
+	// В противном случае, если Query() вернет ошибку, это приведет к панике
+	// так как он попытается закрыть набор результатов у которого значение: nil.
+	defer rows.Close()
+
+	// Инициализируем пустой срез для хранения объектов models.Snippets.
+	var snippets []*models.Snippet
+
+	// Используем rows.Next() для перебора результата. Этот метод предоставляет
+	// первую, а затем каждую следующую запись из базы данных, для обработки
+	// методом rows.Scan().
+	for rows.Next() {
+		s := &models.Snippet{}
+
+		err := rows.Scan(&s.ID, &s.Title, &s.Content, &s.CreatedAt, &s.ExpiresAt)
+		if err != nil {
+			return nil, err
+		}
+
+		snippets = append(snippets, s)
+	}
+
+	// Когда цикл rows.Next() завершается, вызываем метод rows.Err(), чтобы узнать
+	// не возникла ли какая либо ошибка в ходе работы.
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return snippets, nil
 }
